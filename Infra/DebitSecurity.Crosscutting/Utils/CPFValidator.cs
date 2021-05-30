5e8df5ca92158144b09cccbcb6b571adc230eec2
Luiz Nakahara <luizjunior2005@gmail.com>
2021-05-30 10:43:46 -0300

    Infra - Validador de CPF e mensagens de validação

---
+++ Infra/DebitSecurity.Crosscutting/Utils/CPFValidator.cs
@@ -0,0 +1,50 @@
+namespace DebitSecurity.Crosscutting.Utils
+{
+    public class CPFValidator
+    {
+        public static bool Validate(string cpf)
+        {
+            if (string.IsNullOrWhiteSpace(cpf))
+                return false;
+            
+            int[] multiplicator1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicator2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            cpf = cpf.Trim().Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+                return false;
+
+            for (int j = 0; j < 10; j++)
+                if (j.ToString().PadLeft(11, char.Parse(j.ToString())) == cpf)
+                    return false;
+
+            string tempCpf = cpf.Substring(0, 9);
+            int sum = 0;
+
+            for (int i = 0; i < 9; i++)
+                sum += int.Parse(tempCpf[i].ToString()) * multiplicator1[i];
+
+            int rest = sum % 11;
+            if (rest < 2)
+                rest = 0;
+            else
+                rest = 11 - rest;
+
+            string digit = rest.ToString();
+            tempCpf = tempCpf + digit;
+            sum = 0;
+            for (int i = 0; i < 10; i++)
+                sum += int.Parse(tempCpf[i].ToString()) * multiplicator2[i];
+
+            rest = sum % 11;
+            if (rest < 2)
+                rest = 0;
+            else
+                rest = 11 - rest;
+
+            digit = digit + rest.ToString();
+
+            return cpf.EndsWith(digit);
+        }
+    }
+}
---
+++ Infra/DebitSecurity.Crosscutting/Utils/ValidationMessages.cs
@@ -0,0 +1,9 @@
+namespace DebitSecurity.Crosscutting.Utils
+{
+    public static class ValidationMessages
+    {
+        public const string RequiredField = "Campo Obrigatório!";
+        public const string FieldNullOrEmpt = "Campo vazio!";
+        public const string InvalidCPF = "CPF inválido!";
+    }
+}
