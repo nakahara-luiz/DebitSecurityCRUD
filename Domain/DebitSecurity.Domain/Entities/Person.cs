43bda52094adf4a8f46dd6cf49fda49d268e6fc2
Luiz Nakahara <luizjunior2005@gmail.com>
2021-06-01 18:55:36 -0300

    ajuste de modelagem, para redundancia

--- Domain/DebitSecurity.Domain/Entities/Person.cs
+++ Domain/DebitSecurity.Domain/Entities/Person.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+
 namespace DebitSecurity.Domain.Entities
 {
     public class Person: BaseEntity
     {
         public string Name { get; set; }   
         public string CPF { get; set; }
+        public IList<Debit> Debits { get; set; }
     }
 }
